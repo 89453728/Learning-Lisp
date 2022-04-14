b473ff4fcc8be9f84328c955538bb18e12a9fef5
Manolito <manolito@local>
2022-04-14 20:02:10 +0200

    Incluye un fichero mínimo de configuración para Emacs.

---
+++ Examples/recommended-minimal-configuration.el
@@ -0,0 +1,43 @@
+;;; recommended-minimal-configuration.el --- Provides minimal recommended configuration
+
+;; Homepage: https://github.com/89453728/Learning-Lisp.git
+;; Version: 0
+;; Package-Requires: ((use-package "0.1"))
+
+;;; Commentary:
+;; This are recommended bare-minimal configuration. Keep in mind that
+;; depends on `use-package' by jwiegley. More over on
+;; https://github.com/jwiegley/use-package
+
+;;; Code:
+
+(require 'use-package)
+
+;; Relative line number.
+;; Only enabled for programming major modes.
+(setq display-line-numbers-type 'relative)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Visual line mode shows a line wrapped if the line is longer than
+;; window size. you will see a visual indication that the line has
+;; been wrapped.
+(use-package simple
+  :diminish visual-line-mode global-visual-line-mode
+  :config
+  (global-visual-line-mode 1))
+
+;; `show-paren-mode' hightlight the matching paren
+(show-paren-mode)
+
+;; Change `yes or no' dialog to `y or n'.
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Enables `which-key-mode' that helps you to know which key have you
+;; available once you start a key chore.
+(use-package which-key
+  :diminish
+  :config (which-key-mode))
+
+(provide 'recommended-minimal-configuration)
+
+;;; recommended-minimal-configuration.el ends here
