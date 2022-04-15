b7386dcdf4993fbe7a174cd875c149077ecd6f10
texaco <manolo.texaco@gmail.com>
2022-04-15 19:32:02 +0200

    Update `update_index.sh` to ignore Emacs backup files

--- update_index.sh
+++ update_index.sh
@@ -1,6 +1,4 @@
 # updating index file name
 
 printf "This file must have the tree file system.\nIf you're in linux, write tree, copy and paste here\n\n" > INDEX
-tree >> INDEX
-
-
+tree -I "*~" >> INDEX
